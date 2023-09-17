22203d8bde3f2685607d9cfbc205b8f91b234149
zappaciato <58880515+zappaciato@users.noreply.github.com>
2023-09-17 20:11:44 +0200

    Commit with additional options and comments for training.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,22 +3,32 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Interfaces\UserInterface;
 use App\Repository\UserRepository;
 use App\Service\UserServices;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
 
+    // private $userServices;
+
+    // public function __construct(UserServices $userServices)
+    // {
+    //     $this->userServices = $userServices;
+    // } 
+    //:::::  PYtanie czy tak mozna zrobic i potem odwolywac sie do tego za pomoca $this->userServices... czy jest jakas roznica? 
+
     #[Route('/users', name: 'app_users')]
     public function index(UserServices $userServices): JsonResponse
     {
         $users= $userServices->getAllUsers();
-
+        var_dump($users);
         return $this->json([
             $users
         ]);
@@ -28,7 +38,7 @@ class UserController extends AbstractController
     public function getUserById(UserServices $userServices, int $id): JsonResponse
     {
         $user = $userServices->getUserById($id);
-        print_r($user);
+
         return $this->json([
             $user
         ]);
@@ -36,21 +46,47 @@ class UserController extends AbstractController
 
 
     #[Route('/create-user ', name: 'create_user')]
-    public function createUser(EntityManagerInterface $entityManager) : Response {
-        
-        $user = new User();
-        $user->setFirstName('Bruce');
-        $user->setLastName('Willis');
-        $user->setEmail('bw@gmail.com');
-        $user->setPhoneNumber(334444);
-        $user->setRole('user');
+    public function createUser(UserServices $userServices, Request $request) : Response {
 
-        $entityManager->persist($user);
+        $data = [
+            'firstName'     =>  $request->get('firstName'), 
+            'lastName'      =>  $request->get('lastName'), 
+            'email'         =>  $request->get('email'), 
+            'phoneNumber'   =>  $request->get('phoneNumber'),
+        ];
 
-        $entityManager->flush();
-
-        return new Response('User saved in the DB! ' . $user->getEmail());
+        if ($userServices->createUser($data)) {
+            return new Response('User saved in the DB! ');
+        } else {
+            return new Response('User NOT SAVED IN THE DB!!!');
+        };
 
     }
+
+    // to do input point as JSON
+    // #[Route('/create-user ', name: 'create_user')]
+    // public function createUser(UserServices $userServices, Request $request): Response
+    // {
+    //     $request = 
+    //     $userData = json_decode($request->getContent(), true);
+
+    //     if (json_last_error() !== JSON_ERROR_NONE) {
+    //         return new Response('Invalid JSON', Response::HTTP_BAD_REQUEST);
+    //     }
+
+    //     //serialize
+
+    //     //validate;
+
+    //     $user = $userServices->createUser(
+    //         $userData['firstName'],
+    //         $userData['lastName'],
+    //         $userData['email'],
+    //         $userData['phoneNumber']
+    //     );
+
+
+    //     return new Response('User saved in the DB! ' . $user->getEmail());
+    // }
 
 }
--- src/Service/UserServices.php
+++ src/Service/UserServices.php
@@ -3,16 +3,29 @@
 namespace App\Service;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManager;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\VarDumper\VarDumper;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class UserServices 
 {
     /**
      * @param UserRepository $userRepository
+     * @param EntityManagerInterface $entityManager
      */
-    public function __construct(readonly private UserRepository $userRepository)
+    private $entityManager;
+    
+    public function __construct(readonly private UserRepository $userRepository, EntityManagerInterface $entityManager)
     {
-        
+        // $this->userRepository = $userRepository;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -46,6 +59,144 @@ class UserServices
     public function getUserById (int $id): ?User
     {
         return $this->userRepository->find($id) ?? null;
+    }
+
+
+    private function setUserData($data) 
+    {
+        $user = new User();
+        $user->setFirstName($data['firstName']);
+        $user->setLastName($data['lastName']);
+        $user->setEmail($data['email']);
+        $user->setPhoneNumber($data['phoneNumber']);
+
+        if ($this->extractRoleFromPhoneNumber($data['phoneNumber']) === 666) {
+            $user->setRole('ADMIN');
+        } else {
+            $user->setRole('USER');
+        }
+
+        return $user;
+    }
+    // public function createUser($firstName, $lastName, $email, $phoneNumber) {
+
+    public function createUser($data)
+    {
+        $user = $this->setUserData($data);
+
+        $validationErrors = $this->validateUserData($data);
+
+        //validate;
+        if (!empty($validationErrors)) {
+
+                foreach ($validationErrors as $error) {
+                    echo $error;
+                }
+
+            // return new JsonResponse(['errors' => $validationErrors], Response::HTTP_BAD_REQUEST);
+            return false;
+
+        } else {
+            // Persist the user entity to the database
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            return true ;
+        }
+
+        // serialize
+
+
+        // $userSerialized = $this->serializeData($user);
+        // var_dump($userSerialized->firstName);
+
+        // return $user;
+    }
+
+    private function extractRoleFromPhoneNumber($phoneNumber) 
+    {
+
+        $string = (string)$phoneNumber;
+        $result = (int)substr($string, 0, 3);
+        
+        return $result;
+    }
+
+    public function validateUserData(array $data) : array 
+    {
+
+        $validationErrors = [];
+
+        // Validate the firstName
+        if (empty($data['firstName'])) {
+
+            $validationErrors[] = 'First name is required!';
+
+        }
+
+        // Validate the lastName
+        if (empty($data['lastName'])) {
+
+            $validationErrors[] = 'Last name is required';
+
+        }
+
+        // Validate the email address
+        if (empty($data['email'])) {
+
+            $validationErrors[] = 'Email is required';
+
+        } elseif (!$this->isValidEmail($data['email'])) {
+
+            $validationErrors[] = 'Invalid email address';
+
+        }
+
+        // Validate the phoneNumber
+        if (empty($data['phoneNumber'])) {
+
+            $validationErrors[] = 'Phone number is required!';
+
+        } 
+        if (!$this->isValidPhoneNumber($data['phoneNumber'])) 
+        {
+            $validationErrors[] = 'Phone number must be 6 characters long!';
+        }
+
+        return $validationErrors;
+    }
+
+    private function isValidEmail($email)
+    {
+        // Use a simple regex to check if the email format is valid
+        return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+    }
+
+    private function isValidPhoneNumber($phoneNumber)
+    {
+
+        if (strlen((string)$phoneNumber) !== 6) {
+            
+            return false;
+
+        } else {
+
+            return true;
+
+        }
+
+    }
+
+    public function serializeData($data) {
+        
+        $encoders = [new XmlEncoder(), new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $jsonContent = $serializer->serialize($data, 'json');
+
+        return $jsonContent;
+
     }
     
 }

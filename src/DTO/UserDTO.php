cdac26aa14851b5abe067e374119ab18f167cb6c
zappaciato <58880515+zappaciato@users.noreply.github.com>
2023-09-27 16:44:00 +0200

    polulate without nested loop version 1

--- src/DTO/UserDTO.php
+++ src/DTO/UserDTO.php
@@ -11,27 +11,66 @@ class UserDTO {
 
 
 // poniżej Homework do funkcji populate;
-    public function populate(array $params) {
+    // public function populate(array $params) {
 
-        $objectVars = array_keys(get_object_vars($this));
+    //     $objectVars = array_keys(get_object_vars($this));
 
-        foreach ($objectVars as $objectParam) {
+    //     foreach ($objectVars as $objectParam) {
 
-            foreach ($params as $param => $value) {
+    //         foreach ($params as $param => $value) {
 
+    //             if ($objectParam == $param) {
 
-                if ($objectParam == $param) {
+    //                 $this->{$objectParam} = $value;
 
-                    $this->{$objectParam} = $value;
+    //             }
 
-                }
+    //         }
 
-            }
+    //     }
 
+    //     return $this;
+
+    // }
+
+    // poniżej Homework do funkcji populate;
+    // public function populate(array $params)
+    // {
+
+    //     $objectVars = array_keys(get_object_vars($this));
+
+    //     $i = 0;
+
+    //         foreach ($params as $param => $value) {
+
+    //             if ($objectVars[$i] == $param && $i < count($objectVars)) {
+
+    //                 $this->{$objectVars[$i]} = $value;
+
+    //                 $i++;
+
+    //             }
+
+                
+
+    //     }
+
+    //     return $this;
+    // }
+
+    public function populate(array $params)
+    {
+        foreach (get_object_vars($this) as $key => $objectParam) {
+
+            if(isset($params[$key]) && $this->{$key} = $params[$key]) {
+
+                $this->{$key} = $params[$key];
+
+            };
+            
         }
 
         return $this;
-
     }
 
 }
